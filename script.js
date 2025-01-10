1a2c7888f45db88e0f46c88e2af7977cb002e2a3
zooch-btw <526SDiPalma@frhsd.com>
2025-01-10 11:54:09 -0500

    day 3 done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let finalmsg = document.getElementById("endtxt");
 
 let userName = prompt("Hi! Who are you?");
 userName
@@ -19,7 +20,55 @@ function Reset() {
   window.location.reload();
 }
 
-// plays noise on click I WILL WORK ON THIS LATER
+/*CodeAcademy 's code for the Rock, Paper, Scissors game*/
 
-const audio = new Audio("video1.mp3");
-audio.play();
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    console.log("This game is a tie!");
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      console.log("Sorry, computer won!");
+    } else {
+      console.log("Congratualtions, you won!");
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "Sorry, computer won!";
+    } else {
+      return "Congratualtions, you won!";
+    }
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Sorry, computer won!";
+    } else {
+      return "Congratualtions, you won!";
+    }
+  }
+
+  if (userChoice === "bomb") {
+    return "Congratualtions, you won!";
+  }
+};
+
+const playGame = (userChoice) => {
+  const computerChoice = getComputerChoice();
+  //update the HTML to how computer choice
+  //udate HTML to show userChoice
+  determineWinner(userChoice, computerChoice);
+};
